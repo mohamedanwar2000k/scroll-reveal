b93ee1b9b4c9d524e50225447bba653bdc043400
mohamedanwar2000k <153559150+mohamedanwar2000k@users.noreply.github.com>
2024-11-13 22:53:15 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ const revealElement = document.querySelectorAll("[data-reveal]");
 
 const scrollReveal = () => {
   for (let i = 0; i < revealElement.length; i++) {
-    const isElementOnScreen = (revealElement[i].getBoundingClientRect().top < window.innerHeight)
-      && (revealElement[i].getBoundingClientRect().top > -revealElement[i].getBoundingClientRect().height);
+    const isElementOnScreen = (revealElement[i].getBoundingClientRect().top < window.innerHeight);
 
     if (isElementOnScreen) {
       revealElement[i].classList.add("revealed");
